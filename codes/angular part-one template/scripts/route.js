358fc249bc6ddedcf23de1da307c93910d66c93c
zhao-li- <1216186168@qq.com>
2014-05-24 14:07:03 +0800

    template view

--- codes/angular part-one template/scripts/route.js
+++ codes/angular part-one template/scripts/route.js
@@ -13,6 +13,10 @@ angular
                 templateUrl: 'views/main.html',
                 controller: 'MainCtrl'
             })
+//            .when('/此处写URL', {
+//                templateUrl: '此处写要加载的页面的路径',
+//                controller: '此处写控制器，在“order_details_controller.js”文件的第4行可以找到'
+//            })
             .when('/name', {
                 templateUrl: 'views/name.html',
                 controller: 'name'

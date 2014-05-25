6fd7fca117ebbd13ba08aab7d0c6e1c5ac04331f
zhao-li- <1216186168@qq.com>
2014-05-25 15:55:28 +0800

    section1

---
+++ section_angular: template view/test.js
@@ -0,0 +1,8 @@
+angular.module('orderMasterApp')
+    .controller('ChoosePersonCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });

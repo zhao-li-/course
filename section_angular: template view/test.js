0c5bb306e8f5246d9c7bd23d02b82454bf4d8a8f
zhao-li- <1216186168@qq.com>
2014-05-25 18:18:47 +0800

    modify the name

--- section_angular: template view/test.js
+++
@@ -1,8 +0,0 @@
-angular.module('orderMasterApp')
-    .controller('ChoosePersonCtrl', function ($scope) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
-    });
